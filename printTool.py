c6cb120c514185f0812d312f4095e59fd10723c8
kaue <kaueh05@gmail.com>
2025-02-14 10:46:53 -0300

    len(borda) > 1

--- printTool.py
+++ printTool.py
@@ -9,27 +9,30 @@ def deColorLen(texto):
 
 class configCentralizar:
 
-    def __init__(self, MX, borda):
+    def __init__(self, MX: int, borda: str):
         self.MX = MX
         self.borda = borda
 
-    def centralizar(self, texto, ):
+    def centralizar(self, texto: str, ):
         tamanhoTexto = deColorLen(texto)
         fix = 0
         if tamanhoTexto % 2 != 0: fix = 1
 
         espacos = int((self.MX - tamanhoTexto) / 2)
-        print(f"{self.borda * espacos} {texto} {self.borda * (espacos + fix)}")
+        if len(self.borda) > 1:
+            print(f"{(self.borda * espacos)[:espacos]} {texto} {(self.borda * espacos)[espacos - fix:]}")
+
+        else:
+            print(f"{self.borda * espacos} {texto} {self.borda * (espacos + fix)}")
 
 
 class configColorizar:
-    def __init__(self, cor, autoPrint=False):
+    def __init__(self, cor: str, autoPrint: bool = False):
         if cor not in ['rosa', 'azul', 'verde', 'amarelo', 'vermelho', 'clear']:
             raise CorInexistente
 
         self.cor = cor
         self.autoPrint = autoPrint
-
 
     def colorizar(self, texto):
         cores = {'rosa': '\033[95m', 'azul': '\033[94m', 'verde': '\033[92m', 'amarelo': '\033[93m',
@@ -48,3 +51,4 @@ class configColorizar:
 
 class CorInexistente(Exception):
     pass
+
